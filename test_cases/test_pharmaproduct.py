9dce096bad775e39fbecd9f368322a02de88f45d
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-12 14:56:21 +0530

    Adding the proper comments

--- test_cases/test_pharmaproduct.py
+++ test_cases/test_pharmaproduct.py
@@ -21,22 +21,18 @@ class Test_pharma:
         self.driver = setup
         self.wait = WebDriverWait(self.driver, 10)
         self.pharma = Pharma(self.driver)
-
         # Search for the product and click on it
         self.pharma.search_product_name(self.product_name)
         self.products = self.wait.until(EC.visibility_of_all_elements_located(self.ALL_PRODUCTS_NAME))
-
         for self.product in self.products:
             if self.product.text == self.product_name:
                 self.product.click()
                 break
-
         # Add the product to the cart, select maximum quantity, and navigate to the cart
         self.pharma.click_on_add_to_cart_button()
         self.pharma.select_max_value()
         self.pharma.click_on_cart()
-
-        # Retrieve and print the price of the product and the cart total price
+      # Retrieve and print the price of the product and the cart total price
         self.price = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.PRICE_OF_PRODUCT_BY_CSS))).text
         print(self.price)
         self.products_price = self.price.replace("₹", "")
@@ -44,17 +40,14 @@ class Test_pharma:
         self.cart_price = self.wait.until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, self.CART_TOTAL_PRICE_BY_CSS))).text
         print(self.cart_price)
-
         # Assert that the product price matches the cart total price
         assert self.price == self.cart_price
-
         # Remove the product from the cart
         self.pharma.remove_the_price()
 
     def test_title_of_page(self, setup):
         # Initialize WebDriver
         self.driver = setup
-
         # Retrieve and assert the page title
         title = self.driver.title
         assert title == self.driver.title
--- test_cases/test_product_adding_to_cart.py
+++ test_cases/test_product_adding_to_cart.py
@@ -11,13 +11,11 @@ class Test_new:
         # Initialize page objects
         self.base_page = Basepage(self.driver)
         self.pharma = Pharma(self.driver)
-
         # Search for a product and add it to the cart
         self.pharma.searching_product_name("Ecosprin 75mg Strip Of 14 Tablets")
         self.pharma.all_products_list_click_product()
         self.pharma.click_add_to_cart_btn()
         self.pharma.click_o_carts()
-
         # Compare the product price in the cart with the product price
         self.pharma.comparing_the_price()
         assert self.pharma.cart_price == self.pharma.price, "Price does not match"
@@ -25,7 +23,6 @@ class Test_new:
     def test_02_check_the_title(self):
         # Initialize page objects
         self.base_page = Basepage(self.driver)
-
         # Verify the title of the page
         assert self.base_page.get_title() == "PharmEasy: Online Pharmacy & Medical Store in India | 50 Lakhs+ Customers."
 
@@ -33,7 +30,6 @@ class Test_new:
         # Initialize page objects
         self.base_page = Basepage(self.driver)
         self.pharma = Pharma(self.driver)
-
         # Check if the logo is displayed on the page
         assert self.base_page.is_display(self.pharma.CHECK_LOGO_IS_VISIBLE) == True
 
@@ -41,14 +37,12 @@ class Test_new:
         # Initialize page objects
         self.base_page = Basepage(self.driver)
         self.pharma = Pharma(self.driver)
-
         # Search for a product, add it to the cart, and then remove it
         self.pharma.searching_product_name("Ecosprin 75mg Strip Of 14 Tablets")
         self.pharma.all_products_list_click_product()
         self.pharma.click_add_to_cart_btn()
         self.pharma.click_o_carts()
         self.pharma.remove_the_product_form_cart()
-
         # Verify that the product is removed from the cart
         assert (self.base_page.get_text(self.pharma.CHECK_AFTER_REMOVING_PRODUCT_TEXT_CSS)
                 == "0 Items in your Cart"), ("Product is still in your cart", self.base_page.take_screenshot("removed1"))
